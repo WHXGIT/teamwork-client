fc38d6049c6912b943abc8bc828b3d077da9a412
Andy <wanghaoxin5277@gmail.com>
2020-09-18 17:37:25 +0800

    init

---
+++ src/main/java/com/kingdee/inte/teamworkclient/Application.java
@@ -0,0 +1,12 @@
+package com.kingdee.inte.teamworkclient;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+	    System.setProperty("mail.mime.splitlongparameters", "false");
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ src/main/java/com/kingdee/inte/teamworkclient/web/controller/DemoController.java
@@ -0,0 +1,57 @@
+package com.kingdee.inte.teamworkclient.web.controller;
+
+import com.kingdee.inte.teamworkclient.common.ViewResult;
+import com.kingdee.inte.teamworkclient.pojo.Demo;
+import com.kingdee.inte.teamworkclient.service.DemoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * @author Andy
+ * @version 1.0
+ * @date 02/28/2019 16:06
+ * @description 示例TODO-list操作
+ */
+@RestController
+@RequestMapping("/demos")
+public class DemoController {
+
+	@Autowired
+	private DemoService demoService;
+
+	@PostMapping("/demo")
+	public ViewResult add(@RequestBody Demo demo) {
+		ViewResult viewResult = ViewResult.instance();
+		viewResult.setCode(demoService.create(demo));
+		return viewResult;
+	}
+
+	@GetMapping("/demo")
+	public ViewResult find(@RequestParam("id") String id) {
+		ViewResult viewResult = ViewResult.instance();
+		viewResult.setData(demoService.readById(id));
+		return viewResult;
+	}
+
+	@PutMapping("/demo")
+	public ViewResult update(@RequestBody Demo demo) {
+		ViewResult viewResult = ViewResult.instance();
+		viewResult.setCode(demoService.update(demo));
+		return viewResult;
+	}
+
+	@DeleteMapping("demo")
+	public ViewResult delete(@RequestParam("id") String id) {
+		ViewResult viewResult = ViewResult.instance();
+		viewResult.setCode(demoService.deleteById(id));
+		return viewResult;
+	}
+
+	@GetMapping("/")
+	public ViewResult list(@RequestParam("pageNum") Integer pageNum, @RequestParam("pageSize") Integer pageSize) {
+		ViewResult viewResult = ViewResult.instance();
+		viewResult.setData(demoService.list(pageNum, pageSize));
+		return viewResult;
+	}
+
+}
